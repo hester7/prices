461bc26a1da105c4489adbef07b2c0a0091c0b97
hester7 <shester@edms-llc.com>
2023-04-30 07:54:07 -0500

    seed prices

--- src/prices-backend/Prices.PriceUpdater/PriceUpdaterService.cs
+++ src/prices-backend/Prices.PriceUpdater/PriceUpdaterService.cs
@@ -103,7 +103,7 @@ namespace Prices.PriceUpdater
 
         private async Task SeedPricesAsync(CancellationToken cancellationToken)
         {
-            var startYear = !Debugger.IsAttached ? DateTime.Today.Year - 2 : DateTime.Today.Year - 1;
+            var startYear = DateTime.Today.Year - 1;
             await _seedPrices.Run(startYear, cancellationToken: cancellationToken);
         }
 
