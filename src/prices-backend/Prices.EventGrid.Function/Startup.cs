1734d4907f1a1ab6f7e2415aa64b13079c219514
hester7 <shester@edms-llc.com>
2023-12-15 08:28:56 -0600

    logging

--- src/prices-backend/Prices.EventGrid.Function/Startup.cs
+++ src/prices-backend/Prices.EventGrid.Function/Startup.cs
@@ -11,6 +11,7 @@ using Prices.Persistence;
 using Serilog;
 using Serilog.Events;
 using Serilog.Exceptions;
+using Serilog.Sinks.ApplicationInsights.TelemetryConverters;
 using Serilog.Sinks.SystemConsole.Themes;
 
 [assembly: FunctionsStartup(typeof(Startup))]
@@ -30,6 +31,7 @@ internal class Startup : FunctionsStartup
                 theme: AnsiConsoleTheme.Literate,
                 outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} ({SourceContext}){NewLine}{Exception}"
             )
+            .WriteTo.ApplicationInsights("86bc4e5b-e36e-472e-8533-87f72cfb1da9", new TraceTelemetryConverter())
             .CreateLogger();
 
         builder.Services.AddLogging(logging => logging.AddSerilog(logger));
